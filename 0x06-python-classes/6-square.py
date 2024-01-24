6772e0a15c26125d26a0c67fd84c6b158a2e04ae
Mariam Moustafa <mariam.moustafa57@gmail.com>
2024-01-24 01:34:31 -0800

    Classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
+
 class Square:
     """Represent a square."""
 
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
         Args:
             size (int): The size of the new square.
